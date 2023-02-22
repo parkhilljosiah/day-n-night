33ac2d77d8dbd790908863cfa2d32733e34136c6
Josiah Parkhill <parkhilljosiah@gmail.com>
2023-02-21 20:10:55 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  let [theme, setTheme] = useState("light");
+
+  function toggleTheme() {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-container">
+      <div className={`${theme}`}>
+          <button onClick={toggleTheme} className="toggle-button">
+            {theme === "light" ? <i class="bi bi-moon-fill"></i> : <i class="bi bi-sun-fill"></i>}
+          </button>
+        <h1>Hello World.</h1>
+      </div>
     </div>
   );
 }
